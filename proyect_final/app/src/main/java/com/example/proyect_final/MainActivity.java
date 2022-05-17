2ef061fcb8824b5b313add35e81a5c4971ebfd66
ENRIQUE ACOSTA RIVERA <enriqueacostarivera@comunidad.unam.mx>
2022-05-16 21:29:16 -0500

    Se agregó un Listener para el Switch. De momento solo indica si el foco esta prendido o apagado

--- proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.proyect_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch switch1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b){
+                    Toast.makeText(MainActivity.this,"Light on",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Light off",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
     }
 }
