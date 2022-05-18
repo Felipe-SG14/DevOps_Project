b7f61ca7bb379214b7952612abfb82be9c868fe9
Juan Manuel Zuleta <juan.zuleta1999@gmail.com>
2022-05-18 00:55:48 -0500

    (3h, 3i) No se puede corroborar conexión exitosa al Google API CLient y no se pudo conocer la ubicación

--- proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
@@ -3,26 +3,43 @@ package com.example.proyect_final;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.OnConnectionFailedListener {
 
     Switch switch1;
+
+    public GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private TextView mLatitude;
+    private TextView mLongitude;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
+        // Establecer punto de entrada para la API de ubicación
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         switch1 = (Switch) findViewById(R.id.switch1);
@@ -30,11 +47,10 @@ public class MainActivity extends AppCompatActivity implements
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-                if(b){
-                    Toast.makeText(MainActivity.this,"Light on",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this,"Light off",Toast.LENGTH_SHORT).show();
+                if (b) {
+                    Toast.makeText(MainActivity.this, "Light on", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Light off", Toast.LENGTH_SHORT).show();
                 }
 
 
@@ -44,7 +60,23 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitude.setText(String.valueOf(mLastLocation.getLongitude()));
+        } else {
+            Toast.makeText(this, "Ubicación no encontrada", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -55,5 +87,16 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 }
