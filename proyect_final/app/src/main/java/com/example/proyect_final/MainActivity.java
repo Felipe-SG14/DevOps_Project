06eac8543c4155b0737fc21afa65a684adb24772
Juan Muñoz Marán <juanmz@comunidad.unam.mx>
2022-05-19 21:50:36 -0500

    Se implementó el método POST en el script de Android

--- proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MainActivity.java
@@ -7,6 +7,14 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     Switch switch1;
@@ -17,17 +25,75 @@ public class MainActivity extends AppCompatActivity {
         switch1 = (Switch) findViewById(R.id.switch1);
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b){
 
                 if(b){
-                    Toast.makeText(MainActivity.this,"Light on",Toast.LENGTH_SHORT).show();
+                    String url = "http://davinci999.xyz/solicitud.php"; //http://davinci999.xyz
+                    JSONObject jsonObject_foco_on = new JSONObject();
+                    try {
+                        jsonObject_foco_on.put("dispositivo_id",1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        jsonObject_foco_on.put("Estado",1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        jsonObject_foco_on.put("Intensidad",100);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String message = "Light on";
+                    try {
+                        dataUsingVolley(jsonObject_foco_on, url, message);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
                 else{
-                    Toast.makeText(MainActivity.this,"Light off",Toast.LENGTH_SHORT).show();
+                    JSONObject jsonObject_foco_off = new JSONObject();
+                    try {
+                        jsonObject_foco_off.put("dispositivo_id",1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        jsonObject_foco_off.put("Estado",0);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        jsonObject_foco_off.put("Intensidad",0);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String url = "http://davinci999.xyz/solicitud.php"; //http://davinci999.xyz
+                    String message = "Light off";
+                    try {
+                        dataUsingVolley(jsonObject_foco_off, url, message);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-
 
             }
         });
     }
+
+    public void dataUsingVolley(JSONObject jsonObject, String url, String message) throws JSONException {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                response -> {
+                    Toast response_toast = Toast.makeText(getApplicationContext(), "e " + response.toString(), Toast.LENGTH_LONG);
+                    response_toast.show();
+                }, error -> {
+            Toast response_toast = Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG);
+            response_toast.show();
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
 }
