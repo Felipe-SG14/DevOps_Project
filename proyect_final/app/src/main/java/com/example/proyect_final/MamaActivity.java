39b6f5dbf8984f2909327fce0b3427f1659ec509
angeljmz <angel@git.com>
2022-05-26 20:18:29 -0500

    Se agregaron los listeners y acciones para cada switch dentro de MamaActivity.xml

--- proyect_final/app/src/main/java/com/example/proyect_final/MamaActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MamaActivity.java
@@ -4,23 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MamaActivity extends AppCompatActivity {
 
     // Configuración Botones
 
-    public Switch sw_1;
-    public Switch sw_2;
-    public Switch sw_3;
-    public Switch sw_4;
-    public Switch sw_5;
-    public Switch sw_6;
-    public Switch sw_hija;    // Switch 7 es para la hija
-    public Switch sw_hijo;    // Switch 8 es para el hijo
+    public Switch switch1;
+    public Switch switch2;
+    public Switch switch3;
+    public Switch switch4;
+    public Switch switch5;
+    public Switch switch6;
+    public Switch switchHija;    // Switch 7 es para la hija
+    public Switch switchHijo;    // Switch 8 es para el hijo
     public SeekBar sk_hija;
     public SeekBar sk_hijo;
 
@@ -35,14 +45,14 @@ public class MamaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mama);
 
         // Configuración switch y seekbar
-        sw_1 = (Switch)findViewById(R.id.switch1);
-        sw_2 = (Switch)findViewById(R.id.switch2);
-        sw_3 = (Switch)findViewById(R.id.switch3);
-        sw_4 = (Switch)findViewById(R.id.switch4);
-        sw_5 = (Switch)findViewById(R.id.switch5);
-        sw_6 = (Switch)findViewById(R.id.switch6);
-        sw_hija = (Switch)findViewById(R.id.switch7);
-        sw_hijo = (Switch)findViewById(R.id.switch8);
+        switch1 = (Switch)findViewById(R.id.switch1);
+        switch2 = (Switch)findViewById(R.id.switch2);
+        switch3 = (Switch)findViewById(R.id.switch3);
+        switch4 = (Switch)findViewById(R.id.switch4);
+        switch5 = (Switch)findViewById(R.id.switch5);
+        switch6 = (Switch)findViewById(R.id.switch6);
+        switchHija = (Switch)findViewById(R.id.switch7);
+        switchHijo = (Switch)findViewById(R.id.switch8);
 
         sk_hija = (SeekBar)findViewById(R.id.seekBar1);
         sk_hijo = (SeekBar)findViewById(R.id.seekBar2);
@@ -50,8 +60,134 @@ public class MamaActivity extends AppCompatActivity {
         // Permisos de acceso a botones
         rol = getIntent().getStringExtra("dato");
         PermisosRol(rol);
+
+        //Listeners para cada switch
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switch3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switch4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switch5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switch6.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switchHija.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
+        switchHijo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                switchAccion(b);
+            }
+        });
+
     }
 
+
+    //switchAccion(boolean b) ---> Adentro estan las acciones de Verdadero o Falso del Switch
+    private void switchAccion(boolean b){
+        if(b){
+            String url = "https://spiralweb.000webhostapp.com/solicitud.php"; //http://davinci999.xyz
+            JSONObject jsonObject_foco_on = new JSONObject();
+            try {
+                jsonObject_foco_on.put("dispositivo_id",1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                jsonObject_foco_on.put("Estado",1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                jsonObject_foco_on.put("Intensidad",100);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String message = "Light on";
+            try {
+                dataUsingVolley(jsonObject_foco_on, url, message);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            JSONObject jsonObject_foco_off = new JSONObject();
+            try {
+                jsonObject_foco_off.put("dispositivo_id",1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                jsonObject_foco_off.put("Estado",0);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                jsonObject_foco_off.put("Intensidad",0);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String url = "https://spiralweb.000webhostapp.com/solicitud.php"; //http://davinci999.xyz
+            String message = "Light off";
+            try {
+                dataUsingVolley(jsonObject_foco_off, url, message);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void dataUsingVolley(JSONObject jsonObject, String url, String message) throws JSONException {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                response -> {
+                    Toast response_toast = Toast.makeText(getApplicationContext(), "e " + response.toString(), Toast.LENGTH_LONG);
+                    response_toast.show();
+                }, error -> {
+            Toast response_toast = Toast.makeText(MamaActivity.this, message, Toast.LENGTH_LONG);
+            response_toast.show();
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
 
 
     /////////  Función para dar permisos de acceso a los botones //////////////////////////////////////////////////////////////
@@ -66,40 +202,40 @@ public class MamaActivity extends AppCompatActivity {
     {
         if(input.equals("mama"))
         {
-            sw_1.setVisibility(View.VISIBLE);
-            sw_2.setVisibility(View.VISIBLE);
-            sw_3.setVisibility(View.VISIBLE);
-            sw_4.setVisibility(View.VISIBLE);
-            sw_5.setVisibility(View.VISIBLE);
-            sw_6.setVisibility(View.VISIBLE);
-            sw_hija.setVisibility(View.VISIBLE);
-            sw_hijo.setVisibility(View.VISIBLE);
+            switch1.setVisibility(View.VISIBLE);
+            switch2.setVisibility(View.VISIBLE);
+            switch3.setVisibility(View.VISIBLE);
+            switch4.setVisibility(View.VISIBLE);
+            switch5.setVisibility(View.VISIBLE);
+            switch6.setVisibility(View.VISIBLE);
+            switchHija.setVisibility(View.VISIBLE);
+            switchHijo.setVisibility(View.VISIBLE);
             sk_hija.setVisibility(View.VISIBLE);
             sk_hijo.setVisibility(View.VISIBLE);
         }
         if(input.equals("hijo"))
         {
-            sw_1.setVisibility(View.VISIBLE);
-            sw_2.setVisibility(View.VISIBLE);
-            sw_3.setVisibility(View.VISIBLE);
-            sw_4.setVisibility(View.VISIBLE);
-            sw_5.setVisibility(View.VISIBLE);
-            sw_6.setVisibility(View.VISIBLE);
-            sw_hija.setVisibility(View.INVISIBLE);
-            sw_hijo.setVisibility(View.VISIBLE);
+            switch1.setVisibility(View.VISIBLE);
+            switch2.setVisibility(View.VISIBLE);
+            switch3.setVisibility(View.VISIBLE);
+            switch4.setVisibility(View.VISIBLE);
+            switch5.setVisibility(View.VISIBLE);
+            switch6.setVisibility(View.VISIBLE);
+            switchHija.setVisibility(View.INVISIBLE);
+            switchHijo.setVisibility(View.VISIBLE);
             sk_hija.setVisibility(View.INVISIBLE);
             sk_hijo.setVisibility(View.VISIBLE);
         }
         if(input.equals("hija"))
         {
-            sw_1.setVisibility(View.VISIBLE);
-            sw_2.setVisibility(View.VISIBLE);
-            sw_3.setVisibility(View.VISIBLE);
-            sw_4.setVisibility(View.VISIBLE);
-            sw_5.setVisibility(View.VISIBLE);
-            sw_6.setVisibility(View.VISIBLE);
-            sw_hija.setVisibility(View.VISIBLE);
-            sw_hijo.setVisibility(View.INVISIBLE);
+            switch1.setVisibility(View.VISIBLE);
+            switch2.setVisibility(View.VISIBLE);
+            switch3.setVisibility(View.VISIBLE);
+            switch4.setVisibility(View.VISIBLE);
+            switch5.setVisibility(View.VISIBLE);
+            switch6.setVisibility(View.VISIBLE);
+            switchHija.setVisibility(View.VISIBLE);
+            switchHijo.setVisibility(View.INVISIBLE);
             sk_hija.setVisibility(View.VISIBLE);
             sk_hijo.setVisibility(View.INVISIBLE);
         }
