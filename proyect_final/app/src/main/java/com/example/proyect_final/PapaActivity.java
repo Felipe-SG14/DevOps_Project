1e5d4a7110b30b47d64f48d234272f0085c9a0dc
angeljmz <angel@git.com>
2022-05-26 23:22:57 -0500

    Se agregó la función de mostrar la latitud y altitud al presionar un boton en PapaActivity.java

--- proyect_final/app/src/main/java/com/example/proyect_final/PapaActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/PapaActivity.java
@@ -1,17 +1,50 @@
 package com.example.proyect_final;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class PapaActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    int LOCATION_REQUEST_CODE = 1001;
+
+    private double latitude;
+    private double longitude;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_papa);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        Button ubicacionBoton = (Button) findViewById(R.id.button);
+        ubicacionBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLastLocation();
+            }
+        });
+
     }
 
     // Método para regresar a la ventana principal
@@ -20,5 +53,49 @@ public class PapaActivity extends AppCompatActivity {
         Intent regresarMain = new Intent(this, MainActivity.class);
         startActivity(regresarMain);
     }
+
+    /////////  Código de GPS
+
+
+
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+            return;
+        }
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    //We have a location
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+
+                    // Aqui se envia la ubicacion (Latitud  y longitud)
+                    Toast.makeText(PapaActivity.this,
+                            "Latitud: " +String.valueOf(latitude) +
+                           "\n" + "Longitud: " +String.valueOf(longitude), Toast.LENGTH_SHORT).show();
+                    
+
+                } else {
+                    Log.d(TAG, "onSuccess: Location was null...");
+                }
+            }
+        });
+
+        locationTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFaiure: " + e.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    
 
 }
