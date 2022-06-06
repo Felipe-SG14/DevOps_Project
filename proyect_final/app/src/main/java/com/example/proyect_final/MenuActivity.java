2de8a2a1b738dc95ed80124c66959313e108aed0
eduardo03lax <eduardo03lax@yahoo.com.mx>
2022-06-05 19:34:29 -0500

    Se agregó la función que calcula la distancia a la casa, pero, sin usar ubicación GPS real del celular

--- proyect_final/app/src/main/java/com/example/proyect_final/MenuActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MenuActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -15,9 +16,23 @@ public class MenuActivity extends AppCompatActivity {
     public Button ubicacionPadre;
     public Button stopEmergency;
 
+    // Para calcular las distancias
+    // Ubicación origen es la ubicación del celular/hijo/usuario
+    private double latitudOrigen;
+    private double longitudOrigen;
+
+    // Ubicación destino es la ubicación de la casa/destino
+    private double latitudDestino;
+    private double longitudDestino;
+
+    private boolean DistanciaMenor5m;
+
+    // Constantes para calcular la distancia a la casa
+    // Radio Ecuatorial
+    public static final float RadioTierraKm = 6378;
+
     // Variables para asignar roles
     private String rol;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +49,44 @@ public class MenuActivity extends AppCompatActivity {
         rol = getIntent().getStringExtra("dato");
         PermisosRol(rol);
 
+        if (rol.equals("hijo"))
+        {
+            DistanciaAcasa();
+        }
+
+
+    }
+
+    // Función que calcula la distancia de la casa a la ubicación del usuario (hijo)
+    public void DistanciaAcasa()
+    {
+        // Ubicación Destino (en Km)
+        // CASA DEL USUARIO DE RASPBERRY
+        // ACTUALMENTE UBICACIÓN DE LA FACULTAD DE INGENIERÍA
+        latitudDestino = 19.33144;
+        longitudDestino = -99.18408;
+
+        // Ubicación Origen (en Km)
+        // CELULAR DEL USUARIO
+        // ACTUALMENTE UBICACIÓN DE LAS BICIS CERCA DE LA FACULTAD DE INGENIERÍA
+        latitudOrigen = 19.33211;
+        longitudOrigen = -99.18438;
+
+        // Diferencia de latitudes y longitudes (En radianes)
+        double DifLatitud = (latitudDestino-latitudOrigen)*(Math.PI/180);
+        double DifLongitud = (longitudDestino-longitudOrigen)*(Math.PI/180);
+
+        //a = sin²(Δlat/2) + cos(lat1) · cos(lat2) · sin²(Δlong/2)
+        double a = Math.pow(Math.sin(DifLatitud/2),2) + (Math.cos(latitudOrigen))*(Math.cos(latitudDestino))*Math.pow(Math.sin(DifLongitud/2),2);
+
+        //c = 2 · atan2(√a, √(1−a))
+        double c = 2*(Math.atan2(Math.sqrt(a),Math.sqrt(1-a)));
+
+        // d = R · c
+        // En metros
+        double distancia = (RadioTierraKm*c)*1000;
+        String mensajeD = String.valueOf(distancia);
+        Toast.makeText(this, mensajeD, Toast.LENGTH_SHORT).show();
     }
 
     // Función que cambia de la ventana de menú de opciones a la ventana de luces
@@ -51,10 +104,6 @@ public class MenuActivity extends AppCompatActivity {
         Intent i2 = new Intent(this, PapaActivity.class);
         startActivity(i2);
     }
-
-
-
-
 
     public void PermisosRol(String input)
     {
