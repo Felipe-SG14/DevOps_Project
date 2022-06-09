b3cf24b91822a9f2c8f0b8e11dd32a55bead8145
eduardo03lax <eduardo03lax@yahoo.com.mx>
2022-06-08 19:54:07 -0500

    Se agregó la función de GPS y permisos

--- proyect_final/app/src/main/java/com/example/proyect_final/MenuActivity.java
+++ proyect_final/app/src/main/java/com/example/proyect_final/MenuActivity.java
@@ -1,12 +1,22 @@
 package com.example.proyect_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -89,6 +99,77 @@ public class MenuActivity extends AppCompatActivity {
         Toast.makeText(this, mensajeD, Toast.LENGTH_SHORT).show();
     }
 
+    /////////  Código de GPS y permisos //////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLastLocation();
+        } else {
+            askLocationPermission();
+        }
+    }
+
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_REQUEST_CODE);
+            return;
+        }
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    //We have a location
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+
+                    Log.d(TAG, "onSuccess " + location.toString());
+                    Log.d(TAG, "onSuccess " + latitude);
+                    Log.d(TAG, "onSuccess " + longitude);
+
+                } else {
+                    Log.d(TAG, "onSuccess: Location was null...");
+                }
+            }
+        });
+
+        locationTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFaiure: " + e.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.d(TAG, "askLocationPermission: you should show an alert dialog");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Permission granted
+                getLastLocation();
+            } else {
+                //Permission not granted
+            }
+        }
+    }
+
     // Función que cambia de la ventana de menú de opciones a la ventana de luces
     // Para la madre, hijo e hija
     public void abrirMenuLuces(View view) {
@@ -99,7 +180,7 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     // Función que cambia de la ventana de menú de opciones a la ventana para mandar ubicación GPS
-    // Solo para el Padres
+    // Solo para el Padre
     public void abrirMenuUbicacion(View view) {
         Intent i2 = new Intent(this, PapaActivity.class);
         startActivity(i2);
